6e9f334b1a7c276529e17c6f8a466f611b35b4b6
matt-macs <matt.almeidac@gmail.com>
2026-07-02 20:48:11 -0300

    :art: reestrutura classe pra armazenar dados separados de elementos e usar gameData :sparkles: sistema de cavar agora funciona!

--- src/campoMinado.js
+++ src/campoMinado.js
@@ -1,49 +1,175 @@
 export class CampoMinado extends HTMLElement {
-	static get observedAttributes() {
-		return ["cells"]; // vou usar dps
-	}
-	constructor() {
-		super();
-		this.attachShadow({ mode: "open" });
-		this.template = document.createElement("template");
-		this.build();
-	}
+    static get observedAttributes() {
+        return ["cells"]; // vou usar dps
+    }
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.template = document.createElement("template");
 
-	build() {
-		let start = 1;
-		let end = 6;
-		for (let row = start; row < end; row++) {
-			const line = document.createElement("div");
-			line.classList.add("row");
-			line.id = "row" + row;
-			for (let col = start; col < end; col++) {
-				const cell = document.createElement("button");
-				cell.classList.add(`cell`, `row${row}`, `col${col}`);
-				line.appendChild(cell);
-				// console.log(row, col);
-			}
-			this.template.content.appendChild(line);
-		}
-		// console.log(this.template);
-		this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+        this.rows = 8;
+        this.cols = 10;
+        this.bombs = 10;
+        this.board = [];
+    }
 
-		this.gerarMinas(end);
-    this.detectarInputs();
-		this.style();
-	}
+    connectedCallback() {
+        this.fillCampData();
+        this.plantMines();
+        this.countMinesAround();
+        // console.log(this.board);
+        this.build();
+    }
 
-	style() {
-		const style = document.createElement("style");
-		style.textContent = `
+    // back
+
+    fillCampData() {
+        for (let r = 0; r < this.rows; r++) {
+            const row = [];
+            for (let c = 0; c < this.cols; c++) {
+                row.push({
+                    r,
+                    c,
+                    isMine: false,
+                    isDigged: false,
+                    surroundingMines: 0,
+                    isFlagged: false,
+                });
+            }
+
+            this.board.push(row);
+        }
+    }
+
+    plantMines() {
+        for (let i = 0; i < this.bombs; i++) {
+            const row = Math.floor(Math.random() * this.rows);
+            const col = Math.floor(Math.random() * this.cols);
+            this.board[row][col].isMine = true;
+        }
+    }
+
+    countMinesAround() {
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
+                let bombas = 0;
+                if (this.board[r][c].isMine) {
+                    continue;
+                }
+                this.getNeighbors(r, c).forEach((neighbor) => {
+                    if (this.board[neighbor.r][neighbor.c].isMine) {
+                        bombas++;
+                    }
+                });
+                this.board[r][c].surroundingMines = bombas;
+            }
+        }
+    }
+
+    getNeighbors(r, c) {
+        let neighbors = [];
+
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (
+                    r + i < 0 ||
+                    r + i >= this.rows ||
+                    c + j < 0 ||
+                    c + j >= this.cols
+                ) {
+                    continue;
+                }
+                neighbors.push(this.board[r + i][c + j]);
+            }
+        }
+        return neighbors;
+    }
+
+    handleCellClick(cell) {
+        // console.log("clicou!", cell.gameData);
+
+        if (cell.gameData.isFlagged || cell.gameData.isDigged) {
+            return;
+        }
+        if (cell.gameData.isMine) {
+            this.gameOver();
+            return;
+        }
+        this.cavar(cell);
+    }
+    
+    cavar(cell) {
+        if (!cell || !cell.gameData) {
+            return;
+        }
+        if (cell.gameData.isFlagged || cell.gameData.isDigged) {
+            return;
+        }
+
+        cell.classList.add("cavado");
+        cell.gameData.isDigged = true;
+        cell.textContent =
+            cell.gameData.surroundingMines > 0
+                ? cell.gameData.surroundingMines
+                : "";
+
+        if (cell.gameData.surroundingMines === 0) {
+            this.getNeighbors(cell.gameData.r, cell.gameData.c).forEach(
+                (neighbor) => {
+                    if (!neighbor.isMine && !neighbor.isDigged) {
+                        this.cavar(neighbor.element);
+                    }
+                },
+            );
+        }
+    }
+
+    // front
+
+    build() {
+        const gridContainer = document.createElement("div");
+        gridContainer.id = "grid";
+
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
+                const cellData = this.board[r][c];
+                const cellE = document.createElement("button");
+                cellE.classList.add("cell");
+                cellE.gameData = cellData;
+                cellData.element = cellE;
+
+                if (cellData.isMine) {
+                    cellE.classList.add("mine");
+                }
+
+                cellE.addEventListener("click", (e) =>
+                    this.handleCellClick(cellE),
+                );
+                // console.log(row, col);
+                gridContainer.appendChild(cellE);
+            }
+        }
+        // console.log(this.template);
+        // this.template.content.appendChild(gridContainer);
+        this.shadowRoot.appendChild(gridContainer);
+
+        this.style();
+    }
+
+    style() {
+        const style = document.createElement("style");
+        style.textContent = `
       :host {
         display: flex;
         flex-direction: column;
         gap: 0.4rem;
       }
-      .row {
-        display: flex;
+        #grid {
+        display: grid;
+        grid-template-columns: repeat(${this.cols}, 1fr);
+        grid-template-rows: repeat(${this.rows}, 1fr);
         gap: 0.4rem;
-      }
+        }
       .cell {
         border: none;
         border-radius: 0.2rem;
@@ -56,7 +182,7 @@ export class CampoMinado extends HTMLElement {
         cursor: pointer;
       }
       .cell.mine {
-        background-color: #ff0000;
+        background: #ff0000;
       }
       .cell.cavado {
         background: var(--accent-bg);
@@ -66,71 +192,8 @@ export class CampoMinado extends HTMLElement {
         cursor: default;
       }
       `;
-		this.shadowRoot.appendChild(style);
-	}
-
-	gerarMinas(end) {
-		for (let mina = 1; mina < Math.floor(end / 2); mina++) {
-			let row = Math.floor(Math.random() * (end - 1) + 1);
-			let col = Math.floor(Math.random() * (end - 1) + 1);
-      console.log(row, col);
-			this.shadowRoot
-				.querySelector(`.cell.row${row}.col${col}`)
-				.classList.add("mine");
-		}
-	}
-
-  detectarInputs() {
-    const cell = this.shadowRoot.querySelectorAll('.cell');
-    cell.forEach(element => {
-      element.addEventListener('click', () => {
-        this.verificarArredores(element)
-      });
-    });
-  }
-
-  verificarArredores(btn) {
-    if (btn.classList.contains("mine")) {
-      // fim de jogo
-      return;
+        this.shadowRoot.appendChild(style);
     }
-    else {
-      this.cavar(btn);
-    }
-
-    let row = btn.classList[1].slice(3);
-    let col = btn.classList[2].slice(3);
-
-    let bombas = 0;
-    
-    console.log(row, col);
-
-
-    for (let i = -1; i <= 1; i++) {
-      for (let j = -1; j <= 1; j++) {
-        if (i === 0 && j === 0) continue;
-        const adjacentRow = parseInt(row) + i;
-        const adjacentCol = parseInt(col) + j;
-        const adjacentCell = this.shadowRoot.querySelector(`.cell.row${adjacentRow}.col${adjacentCol}`);
-        if(adjacentCell === null) { continue;}
-        if (adjacentCell.classList.contains('cavado')) {
-          continue;
-        }
-        if (adjacentCell && adjacentCell.classList.contains("mine")) {
-          bombas++;
-          
-        }
-        else {
-          this.cavar(adjacentCell);
-        }
-      }
-    }
-    btn.textContent = bombas > 0 ? bombas : '';
-  }
-  
-  cavar(cell) {
-    cell.classList.add('cavado');
-  }
 }
 
 customElements.define("campo-minado", CampoMinado);
